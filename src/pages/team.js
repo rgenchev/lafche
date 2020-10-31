fb87cf7141259838ba278e90cca3b5d5684172cb
Radoslav <r.genchew@gmail.com>
2020-10-31 23:06:33 +0100

    New design for the Team page

--- src/pages/team.js
+++ src/pages/team.js
@@ -9,43 +9,49 @@ const TeamsPage = ({ data }) => (
   <Layout>
     <CustomHelmet page={GatsbyConfig.siteMetadata.team} />
     <div className="page">
-      <div className="container">
-        <h2>Екип</h2>
-        <div className="team-section">
-          {/* Card for coordinators */}
-          <a className="card-link" href="https://www.linkedin.com/in/yosifgenchev" target="_blank" rel="noopener noreferrer">
-            <div className="member-card white-bg elevate">
-              <Img
-                fluid={data.yosifGenchevAvatarImage.childImageSharp.fluid}
-                className="profile-pic"
-                style={{
-                  zIndex: 3
-                }}
-              />
+      <div className="container tc">
+        <h1>Екип</h1>
 
-              <div className="member-details">
-                <h3 className="name">Йосиф Генчев</h3>
-                <p className="designation">Съосновател</p>
-              </div>
+        <div class="mw9 center ph3-ns pt5">
+          <div class="cf ph2-ns">
+            <div class="fl w-100-m w-50-ns pa2">
+              <article class="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+                <div class="tc">
+                  <Img
+                      fluid={data.yosifGenchevAvatarImage.childImageSharp.fluid}
+                      className="br-100 h4 w4 dib"
+                      style={{
+                        zIndex: 3
+                      }}
+                    />
+                  <h1 class="f4">Йосиф Генчев</h1>
+                </div>
+
+                <a href="https://www.linkedin.com/in/yosifgenchev" target="_blank" rel="noopener noreferrer">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
+                </a>
+              </article>
             </div>
-          </a>
 
-          <a className="card-link" href="https://rgenchev.com/" target="_blank" rel="noopener noreferrer">
-            <div className="member-card white-bg elevate">
-              <Img
-                fluid={data.radoslavGenchevAvatarImage.childImageSharp.fluid}
-                className="profile-pic"
-                style={{
-                  zIndex: 3
-                }}
-              />
+            <div class="fl w-100-m w-50-ns pa2">
+              <article class="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+                <div class="tc">
+                  <Img
+                      fluid={data.radoslavGenchevAvatarImage.childImageSharp.fluid}
+                      className="br-100 h4 w4 dib"
+                      style={{
+                        zIndex: 3
+                      }}
+                    />
+                  <h1 class="f4">Радослав Генчев</h1>
+                </div>
 
-              <div className="member-details">
-                <h3 className="name">Радослав Генчев</h3>
-                <p className="designation">Съосновател</p>
-              </div>
+                <a href="https://rgenchev.com/" target="_blank" rel="noopener noreferrer">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6.188 8.719c.439-.439.926-.801 1.444-1.087 2.887-1.591 6.589-.745 8.445 2.069l-2.246 2.245c-.644-1.469-2.243-2.305-3.834-1.949-.599.134-1.168.433-1.633.898l-4.304 4.306c-1.307 1.307-1.307 3.433 0 4.74 1.307 1.307 3.433 1.307 4.74 0l1.327-1.327c1.207.479 2.501.67 3.779.575l-2.929 2.929c-2.511 2.511-6.582 2.511-9.093 0s-2.511-6.582 0-9.093l4.304-4.306zm6.836-6.836l-2.929 2.929c1.277-.096 2.572.096 3.779.574l1.326-1.326c1.307-1.307 3.433-1.307 4.74 0 1.307 1.307 1.307 3.433 0 4.74l-4.305 4.305c-1.311 1.311-3.44 1.3-4.74 0-.303-.303-.564-.68-.727-1.051l-2.246 2.245c.236.358.481.667.796.982.812.812 1.846 1.417 3.036 1.704 1.542.371 3.194.166 4.613-.617.518-.286 1.005-.648 1.444-1.087l4.304-4.305c2.512-2.511 2.512-6.582.001-9.093-2.511-2.51-6.581-2.51-9.092 0z"/></svg>
+                </a>
+              </article>
             </div>
-          </a>
+          </div>
         </div>
       </div>
     </div>
@@ -59,14 +65,14 @@ export const teamQuery = graphql`
     yosifGenchevAvatarImage: file(relativePath: { eq: "images/yge.jpeg" }) {
       childImageSharp {
         fluid(maxWidth: 200) {
-          ...GatsbyImageSharpFluid_tracedSVG
+          ...GatsbyImageSharpFluid
         }
       }
     }
-    radoslavGenchevAvatarImage: file(relativePath: { eq: "images/rgenchev.jpeg" }) {
+    radoslavGenchevAvatarImage: file(relativePath: { eq: "images/rado.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 200) {
-          ...GatsbyImageSharpFluid_tracedSVG
+          ...GatsbyImageSharpFluid
         }
       }
     }
