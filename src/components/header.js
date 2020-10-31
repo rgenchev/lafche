32c8125af22fe06ef514c8de1f18795bd3347087
Radoslav <r.genchew@gmail.com>
2020-11-01 00:40:20 +0100

    Header: stop accepting 'siteTitle' prop

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,8 @@ import Link from 'gatsby-link'
 import favicon from '../images/favicon.ico'
 import Helmet from 'react-helmet'
 import GatsbyConfig from '../../gatsby-config'
-// import SiteLogo from '../images/logo_transparent.png';
 
-const Header = ({ siteTitle }) => (
+const Header = () => (
   <div className="nav-bar">
     <Helmet
       title={GatsbyConfig.siteMetadata.title}
@@ -22,7 +21,6 @@ const Header = ({ siteTitle }) => (
     <div className="container">
       <div className="nav-items">
         <Link to="/" className="brand-link">
-          {/* <img src={SiteLogo} className="brand" alt="Site Logo" /> */}
           <span className="f3 fw5 dib w2 br-100 black">Лафче</span>
         </Link>
         <ul>
